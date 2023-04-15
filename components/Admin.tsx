6555560201501fffc49d8708aee61f6f7de54b9b
ccastri <camilo.castrillon@uao.edu.co>
2023-04-14 19:27:21 -0500

    More animations and UI stuff

--- components/Admin.tsx
+++ components/Admin.tsx
@@ -14,25 +14,31 @@ export interface DivClicked {
 
 
 export const Admin = () => {
-  const [isClicked, setIsClicked] = useState(false)
- 
+  // const [isClickedII, setIsClickedII] = useState<number|null>(null)
+  const [isClicked, setIsClicked] = useState<Boolean>(false)
+  const [isClickedII, setIsClickedII] = useState<number|null>(null)
   const plansRef = useRef<null | HTMLDivElement>(null)
   const handleScroll = () => {
     plansRef.current?.scrollIntoView({behavior: 'smooth'})
   }
 
   const handleClick = ()=>{
+    // setIsClicked(id)
     setIsClicked(!isClicked)
   }
+  const handleImageClick = (id:number)=>{
+    setIsClickedII(id)
+    console.log(isClickedII)
+    // setIsClicked(!isClicked)
+  }
   return (
-    <div className ={`flex  flex-col text-center text-white  h-154 items-center xl:grid grid-cols-4 bg-[#5C7AA5]`} >
-     
+    <div className ={`flex  flex-col text-center text-white  h-154 items-center xl:grid grid-cols-4 bg-[#5C7AA5]`}>
     <div className='w-full  my-auto py-6 items-center  text-center text-3xl font-bold'>
           <p className='text-center'>Seguridad social en 4 componentes:</p>
           </div>
         <div className={`flex  flex-col text-center text-white  w-full h-full items-center xl:grid grid-cols-4`}>
           {AdminItems.map(item=>(
-            <div key={item.title} className={` h-80 w-full relative group  ${item.color} `}>
+            <div key={item.id} className={` h-80 w-full relative group  ${item.color} `}  onClick={() => handleImageClick(item.id)}>
                 
                 <Image
                 src={item.image}
@@ -40,27 +46,31 @@ export const Admin = () => {
                 alt={item.title}
                 className={` z-10 group-hover:opacity-50  `}
                 />
-                <div className={`w-full h-full mx-auto relative z-50  mb-0 opacity-0 group-hover:opacity-100 ${isClicked?'':' pt-16'} justify-center `}>
+                <div className={`w-full h-full mx-auto relative z-50  mb-0 opacity-0 group-hover:opacity-100 ${isClicked?'transition-opacity duration-200':''} justify-center `} onClick= {()=>{handleClick}}>
                   {item.icon}
                   <p 
                   className='flex mx-auto  text-white text-center relative items-center opacity-0 group-hover:opacity-100 text-3xl font-OpenSans justify-center font-bold'>
                     {item.title}
                   </p>
-                  {isClicked ? (
-                    <p  className='font-bold flex  justify-center w-16  mb-2 py-1 px-4 ml-[45%] text-white border-2 border-white  hover:bg-red-500 rounded cursor-pointer'
+                  {
+                  item.id === isClickedII &&
+                  
+                  isClicked ? (
+                    <p  className='font-bold flex  justify-center w-16 transition-opacity duration-800  mb-2 py-1 px-4 ml-[45%] text-white border-2 border-white hover:transition hover:duration-200 hover:bg-red-500 rounded cursor-pointer'
                       onClick={handleClick}>Close</p>):(<p 
-                      className='font-bold border-2 border-white hover:bg-blue-500 w-24 ml-[40%] cursor-pointer rounded-md py-2 mt-2'
+                      className='font-bold border-2 border-white hover:bg-blue-500 w-24 ml-[42%] cursor-pointer transition-shadow duration-200  rounded-md py-2 mt-2'
                       onClick={handleClick}>Ver mas...</p>
                       )}
 
                       {isClicked &&(
-                        <div className=' p-5 h-[50%]  flex flex-column opacity-100  items-start bg-slate-400  w-full   '>
-                          <p className=''>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, fuga deserunt! Voluptatem 
+                        <div className={` h-auto opacity-70 transition-transform duration-700 p-5 ${isClicked ? 'z-30 translate-y-0 ':'translate-y-full duration-500 opacity-0'}
+                        ${isClickedII === item.id ?' ':'' } flex flex-column  items-start bg-slate-400  w-full`} key={isClickedII}>
+                          <p className=' opacity-100 z-50 '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, fuga deserunt! Voluptatem 
                           distinctio dicta recusandae ipsam nulla nisi. Aliquam tempora minima sunt, sapiente quaerat
                           ratione natus temporibus beatae officiis obcaecati.
                             <hr/>
                               <span  onClick={handleScroll}
-                                className='cursor-pointer relative hover:bg-blue-900  border-2 border-white mt-1 ml-[40%] w-28 px-2 font-bold text-xs rounded-md py-2 flex '
+                                className='cursor-pointer  hover:bg-blue-900 z-10000  border-2 border-white mt-2 ml-[41%] w-28 px-2 font-bold text-xs rounded-xs py-2 flex '
                               >Planes y precios
                             </span>
                           </p>
--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -3,25 +3,58 @@ import HomeIcon from '@mui/icons-material/Home';
 import Diversity2Icon from '@mui/icons-material/Diversity2';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import LoginIcon from '@mui/icons-material/Login';
 import { MenuIcon } from '@heroicons/react/outline';
-import {useRef, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 const Sidebar = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(true)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
 
      const HomeRef = useRef<null | HTMLDivElement>(null)
+     const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     // <div className='fixed'>
-
-    <div className={`  fixed z-50  w-full top-0 h-28  text-white text-xl bg-[#675FFA]  `}>
+// !Animated Icon
+    <div className={`  fixed z-50  w-full top-0 h-28  text-white text-xl bg-[#675FFA]  ${ isScrolled ? 'opacity-70' : 'opacity-100'} `}>
         <div className='      flex flex-row w-full justify-between items-center  py-2  '>
 
           {/* <Sidebar/> */}
           <>
-          <MenuIcon 
+           <div className="relative w-8  h-16 mx-8" onClick={()=>{setIsOpen(!isOpen)}}>
+      <div
+        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-0.5 bg-white transition-all duration-200 ${isOpen ? 'rotate-45' : 'mt-2'}`}
+      ></div>
+      <div
+        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-0.5 bg-white transition-all duration-200 ${isOpen ? '-rotate-45' : ''}`}
+      ></div>
+      <div
+        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-0.5 bg-white transition-all duration-200 ${isOpen ? 'opacity-0' : 'mt-4'}`}
+      ></div>
+    </div>
+
+          {/* <MenuIcon 
           onClick={()=> setIsOpen(!isOpen)}
-          className={` z-10 h-10 mx-8 transition duration-100 transform hover:scale-125 hover:text-white`} />
+          className={` z-10 h-10 mx-8 transition duration-100 transform hover:scale-125 hover:text-white`} /> */}
           </>
 {/* <> */}
 <>
@@ -37,37 +70,54 @@ const Sidebar = () => {
           {/* </> */}
           </>
           
+          
           </div>
-    <div className={`fixed z-50 opacity-[85%] w-[55%] top-28 h-[100%]  ${isOpen && '-translate-x-full  bottom-0 absolute'}  rounded bg-blue-500`}>
+    <div className={`fixed z-50 opacity-[85%] w-[55%] top-28 h-[100%] transform ${isOpen ?  'translate-x-all duration-300 ': '-translate-x-full duration-300  bottom-0 '}  rounded bg-blue-500`}>
         <h1 className='text-center text-3xl py-4 '>MENU</h1>
         {/* <Link ></Link> */}
         <div 
         
-        className='  rounded-md hover:opacity-100  text-justify justify-between hover:bg-green-500 space-y-4 p-6'>
+        className='  rounded-md hover:opacity-100   text-justify justify-between  space-y-4 py-6'>
             <Link href='/' onClick={()=>setIsOpen(!isOpen)}>
-            <div className='flex space-x-4 items-center hover:text-3xl m-auto w-full hover:bg-slate-600 rounded-md hover:px-4'>
+            <div className='flex space-x-4 items-center  m-auto w-full hover:text-2xl hover:duration-200 px-4 py-2 hover:bg-slate-600'>
             <HomeIcon sx={{fontSize:'30px'}} className=''/>
             <h2 className=''>Inicio</h2>
             </div>
-            </Link>
-           <div className='flex space-x-4 items-center hover:text-3xl  hover:bg-slate-600 rounded-md hover:px-4'>
+            </Link >
+           <div className='flex space-x-4 items-center hover:text-2xl hover:duration-200 px-4 py-2 hover:bg-slate-600 w-full '>
             <Diversity2Icon sx={{fontSize:'30px'}} className=''/>
             <h2 className=''>Quienes Somos</h2>
             </div>
             <div className='cursor-pointer'>
-            <Link href='/signup'
-            className='flex space-x-4 items-center hover:text-3xl cursor-pointer hover:bg-slate-600 rounded-md hover:px-4'
+            <Link href='/signup' onClick={()=>setIsOpen(!isOpen)}
+            className='flex space-x-4 items-center  cursor-pointer px-4 hover:transition hover:duration-500 hover:bg-slate-600 hover:text-3xl '
             >
             <GroupAddIcon sx={{fontSize:'30px'}} className=''/>
             <h2 className=''>Afiliate Ya</h2>
             </Link>
             </div>
-            <div className='w-full text-center flex space-x-2 items-center hover:text-3xl  hover:bg-slate-600 rounded-md '>
+            <div className='w-full text-center flex space-x-2 items-center hover:text-3xl px-4 hover:bg-slate-600  '>
             <ConnectWithoutContactIcon sx={{fontSize:'30px'}} className=''/>
             <h2 className=''>Contactanos</h2>
             </div>
+            <div className='coursor-pointer'>
+               <Link href='/dashboard' onClick={()=>setIsOpen(!isOpen)}
+               className='w-full text-center flex space-x-2 items-center hover:text-3xl px-4 hover:bg-slate-600  '>
+            <AssignmentIndIcon sx={{fontSize:'30px'}} className=''/>
+            <h2 className=''>Mis usuarios</h2>
+             </Link>
+            </div>
             
         </div>
+
+        <div className='cursor-pointer'>
+          <Link href='/signin'
+        className='w-full mt-[350px]  pl-4 text-center flex space-x-2  hover:text-3xl bottom-0 items-center hover:bg-slate-600  '>
+            <LoginIcon sx={{fontSize:'30px'}} className=''/>
+
+            <h2 className=''>Inicio de sesion</h2>
+        </Link>
+            </div>
         </div>
     </div>
     // </div>
